a0faac6698da78283be88bcda6949f407e17b604
psmacabulos <patrick.macabulos@gmail.com>
2026-01-14 18:58:25 +0800

    Task 1: create simple shell 0.1

--- simple_shell_0.1.c
+++ simple_shell_0.1.c
@@ -1,25 +1,54 @@
-#include <stdio.h>      /* printf(), perror(), getline() */
-#include <stdlib.h>     /* exit(), malloc(), free() */
-#include <unistd.h>     /* fork(), execve() */
-#include <sys/wait.h>   /* wait() */
+#include "main.h"
 
 /**
-* main - Simple Shell 0.1
-*
-* Return: 0 on success, 1 on failure
-*/
-
+ * main - Simple UNIX command line interpreter
+ *
+ * Return: Always 0
+ */
 int main(void)
 {
-	/* define the parameter for getline */
-	char *user_input = NULL;
+    char *user_input = NULL;
+    size_t input_size = 0;
+    ssize_t chars_read;
+    pid_t child_id;
 
-	size_t input_size = 0;
-	ssize_t chars_read;
+    while (1)
+    {
+        write(STDOUT_FILENO, "#cisfun$ ", 9);
 
-	printf(">>> "); /* prompt */
-	chars_read = getline(&user_input, &input_size, stdin);
+        chars_read = getline(&user_input, &input_size, stdin);
+        if (chars_read == -1)
+        {
+            write(STDOUT_FILENO, "\n", 1);
+            break;
+        }
 
-	printf("You typed %s", user_input);
+        if (user_input[chars_read - 1] == '\n')
+            user_input[chars_read - 1] = '\0';
 
+        child_id = fork();
+        if (child_id == -1)
+            continue;
+
+        if (child_id == 0)
+        {
+            char *argv[2];
+
+            argv[0] = user_input;
+            argv[1] = NULL;
+
+            execve(user_input, argv, environ);
+
+            write(STDERR_FILENO,
+                  "./shell: No such file or directory\n", 34);
+            exit(EXIT_FAILURE);
+        }
+        else
+        {
+            wait(NULL);
+        }
+    }
+
+    free(user_input);
+    return (0);
 }
