3133e9f623919ba84ed3055447c20d1fc1a88d8e
psmacabulos <patrick.macabulos@gmail.com>
2026-01-15 19:03:04 +0800

    fix(shell): resolve PATH using argv[0] to support commands with arguments

--- execute_input.c
+++ execute_input.c
@@ -10,22 +10,26 @@ void execute_input(char *user_input, char *shell_name,
 		   unsigned long line_number)
 {
 	pid_t child_id;
-	char *command;
-	char *resolved_path;
+	char *command, *resolved_path;
 	char **argument_list;
 
 	command = handle_input(user_input, strlen(user_input));
 	if (command == NULL)
 		return;
 
-	resolved_path = resolve_command_path(command, shell_name, line_number);
+	argument_list = build_argv(command);
+	if (argument_list == NULL || argument_list[0] == NULL)
+		return;
+
+	resolved_path = resolve_command_path(argument_list[0],
+					     shell_name,
+					     line_number);
 	if (resolved_path == NULL)
 		return;
 
 	child_id = fork();
 	if (child_id == 0)
 	{
-		argument_list = build_argv(command);
 		execve(resolved_path, argument_list, environ);
 		print_error(shell_name, line_number, argument_list[0]);
 		exit(EXIT_FAILURE);
@@ -35,6 +39,6 @@ void execute_input(char *user_input, char *shell_name,
 		wait(NULL);
 	}
 
-	if (resolved_path != command) /* free only if it is dynamically allocated*/
+	if (resolved_path != argument_list[0])
 		free(resolved_path);
 }
